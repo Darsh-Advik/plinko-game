58d649598b9ac2d09ac8d579269000893f0c4351
Darsh-Advik <70957878+Darsh-Advik@users.noreply.github.com>
2020-10-26 18:35:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var ground1;
+
+function setup() {
+  createCanvas(480,800);
+ // createSprite(400, 200, 50, 50);
+
+ground1 = new Ground(200,200,480,20);
+
+}
+
+function draw() {
+  background(0);  
+  //drawSprites();
+  
+}
